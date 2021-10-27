7f68d31ed304dc6a6c9e02e4f1c39889581ec046
Nikos Sarantopoulos <n.sarantopoulos@okaa.local>
2021-10-27 14:31:08 +0300

    my first commit

---
+++ drawer13/app/src/main/java/com/example/jazzLibrary/ArtistAndVideoCount.java
@@ -0,0 +1,46 @@
+package com.example.jazzLibrary;
+
+public class ArtistAndVideoCount {
+
+    private int artist_id;
+    private String artist_name;
+    private String artist_surname;
+    private int instrument_id;
+    private int artist_video_count;
+
+
+
+
+
+    public int getArtist_id() {
+        return artist_id;
+    }
+
+    public void setArtist_id(int artist_id) {
+        this.artist_id = artist_id;
+    }
+
+    public String getArtist_name() {
+        return artist_name;
+    }
+
+    public void setArtist_name(String artist_name) {
+        this.artist_name = artist_name;
+    }
+
+    public String getArtist_surname() {
+        return artist_surname;
+    }
+
+    public void setArtist_surname(String artist_surname) {
+        this.artist_surname = artist_surname;
+    }
+
+    public int getInstrument_id() { return instrument_id; }
+
+    public void setInstrument_id(int instrument_id) { this.instrument_id = instrument_id; }
+
+    public int getArtist_video_count() { return artist_video_count; }
+
+    public void setArtist_video_count(int artist_video_count) { this.artist_video_count = artist_video_count; }
+}
---
+++ drawer13/app/src/main/java/com/example/jazzLibrary/DurationAndVideoCount.java
@@ -0,0 +1,32 @@
+package com.example.jazzLibrary;
+
+public class DurationAndVideoCount {
+
+    private int duration_id;
+    private String duration_name;
+    private int video_count;
+
+    public int getDuration_id() {
+        return duration_id;
+    }
+
+    public void setDuration_id(int duration_id) {
+        this.duration_id = duration_id;
+    }
+
+    public String getDuration_name() {
+        return duration_name;
+    }
+
+    public void setDuration_name(String duration_name) {
+        this.duration_name = duration_name;
+    }
+
+    public int getVideo_count() {
+        return video_count;
+    }
+
+    public void setVideo_count(int durationCount) {
+        this.video_count = durationCount;
+    }
+}
